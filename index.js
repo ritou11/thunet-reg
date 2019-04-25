bd43b41712bb99358a89532b7ba8e9398f9b2f97
haotian <ritou11@gmail.com>
2019-04-25 14:24:41 +0800

    WIP: fix specified ip func

--- index.js
+++ index.js
@@ -3,42 +3,56 @@ const os = require('os');
 const path = require('path');
 const fs = require('fs');
 const yargRoot = require('yargs');
-const { getIp, getMd5 } = require('./utils.js');
+const { getMd5 } = require('./utils.js');
 const ThunetReg = require('./reg');
 
 const thunetReg = new ThunetReg(1000);
 
-const readConfig = ({ configFile, username, password, md5Password, net, ip }) => {
+const readConfig = ({ configFile, username, password, md5Password, ip }) => {
   let config = {};
   try {
     config = JSON.parse(fs.readFileSync(configFile, 'utf8'));
   } catch (e) { config = {}; }
-  const inputConfig = { username, password, md5Password, net, ip };
+  const inputConfig = { username, password, md5Password, ip };
   _.forEach(inputConfig, (value, key) => { config[key] = value || config[key]; });
-  if (ip) config.ip = ip;
-  else if (config.net) {
-    const ips = getIp(config.net);
-    if (!(ips == null || ips.length <= 0)) {
-      [config.ip] = ips;
-    }
-  }
   if (config.password) config.md5Password = getMd5(config.password);
   return config;
 };
 
 const checkConfig = (config) => {
   if (!config.username) return 'No Username';
-  if (!config.md5Password) return 'No Password';
+  if (!config.md5Password && !config.password) return 'No Password';
   return false;
 };
 
 const tryLogin = (config) => {
+  if (config.ip) {
+    thunetReg.login(config.username, config.md5Password).then(
+      ({ data }) => { console.log(data); },
+    ).catch(() => {
+      console.log('Network error! Try again...');
+      setTimeout(() => tryLogin(config), 1000);
+    });
+  } else {
+    thunetReg.reg(config.username, config.md5Password, config.ip).then(
+      ({ data }) => { console.log(data); },
+    ).catch(() => {
+      console.log('Network error! Try again...');
+      setTimeout(() => tryLogin(config), 1000);
+    });
+  }
+};
+
+const tryAuth = (config) => {
   thunetReg.login(config.username, config.md5Password).then(
     ({ data }) => { console.log(data); },
   ).catch(() => {
     console.log('Network error! Try again...');
     setTimeout(() => tryLogin(config), 1000);
   });
+  thunetReg.auth4(config.username, config.password, config.ip).then(
+    ({ data }) => { console.log(data); },
+  );
 };
 
 module.exports = yargRoot
@@ -68,7 +82,7 @@ module.exports = yargRoot
     describe: 'Network of your computer, e.g. en0, eth0, ...',
     type: 'string',
   })
-  .command('reg [<ip>]', 'Register the IP',
+  .command('login [<ip>]', 'Login my current IP',
     (yargs) => {
       yargs
         .positional('ip', {
@@ -83,19 +97,8 @@ module.exports = yargRoot
         console.error(ck);
         return;
       }
-      thunetReg.reg(config.username, config.md5Password, config.ip).then(
-        ({ data }) => { console.log(data); },
-      );
+      tryLogin(config);
     })
-  .command('login', 'Login my current IP', () => {}, (argv) => {
-    const config = readConfig(argv);
-    const ck = checkConfig(config);
-    if (ck) {
-      console.error(ck);
-      return;
-    }
-    tryLogin(config);
-  })
   .command('logout', 'Logout my current IP', () => {}, (argv) => {
     const config = readConfig(argv);
     const ck = checkConfig(config);
@@ -107,28 +110,42 @@ module.exports = yargRoot
       ({ data }) => { console.log(data); },
     );
   })
-  .command('auth', 'Register current IP with auth4', () => {}, (argv) => {
-    const config = readConfig(argv);
-    const ck = checkConfig(config);
-    if (ck) {
-      console.error(ck);
-      return;
-    }
-    thunetReg.auth4(config.username, config.password, config.ip).then(
-      ({ data }) => { console.log(data); },
-    );
-  })
-  .command('unauth', 'Unregister current IP with auth4', () => {}, (argv) => {
-    const config = readConfig(argv);
-    const ck = checkConfig(config);
-    if (ck === 'No Username') {
-      console.error(ck);
-      return;
-    }
-    thunetReg.unauth4().then(
-      ({ data }) => { console.log(data); },
-    ).catch(() => { console.log('Logout.'); });
-  })
+  .command('auth [<ip>]', 'Register [current] IP with auth4',
+    (yargs) => {
+      yargs
+        .positional('ip', {
+          describe: '<ip> Which IP to register.',
+          type: 'string',
+        });
+    },
+    (argv) => {
+      const config = readConfig(argv);
+      const ck = checkConfig(config);
+      if (ck) {
+        console.error(ck);
+        return;
+      }
+      tryAuth(config);
+    })
+  .command('unauth [<ip>]', 'Unregister [current] IP with auth4',
+    (yargs) => {
+      yargs
+        .positional('ip', {
+          describe: '<ip> Which IP to register.',
+          type: 'string',
+        });
+    },
+    (argv) => {
+      const config = readConfig(argv);
+      const ck = checkConfig(config);
+      if (ck === 'No Username') {
+        console.error(ck);
+        return;
+      }
+      thunetReg.unauth4().then(
+        ({ data }) => { console.log(data); },
+      ).catch(() => { console.log('Logout.'); });
+    })
   .command('keeplogin [<delay>]', 'Keep current IP logged in by continuous trying',
     (yargs) => {
       yargs
